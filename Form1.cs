d12f05c1a4e45fbb3aeab3facc7db9960688a26c
Marco Castope <marcocastope7@gmail.com>
2021-04-28 21:27:12 -0500

    se cambia el color del label nombre

--- Form1.cs
+++ Form1.cs
@@ -19,12 +19,12 @@ namespace MoansoTarea2
 
         private void label2_Click(object sender, EventArgs e)
         {
-            labelNombre.ForeColor = Color.Red;
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            labelNombre.ForeColor = Color.Red;
         }
 
         private void btnRegistrar_Click(object sender, EventArgs e)
